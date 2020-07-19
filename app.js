53cc363be0a64bfd537cbcafc3bed90c34ac7f0a
Camille Bailey <59386910+camcoder-1@users.noreply.github.com>
2020-07-18 23:06:03 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 console.log('Sanity Check!')
 
-{/* <script> */}
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-{/* </script> */}
-
 // DOM function that pulls memory cards from html to js
 const cards = document.querySelectorAll('.memory-card');
 const sound = document.getElementById('sound');
@@ -17,8 +10,6 @@ let stopBoard = false;
 let firstCard, secondCard;
 
 
-
-
 // function that checks if cards are match on flip
 function flipCard() {
     if(stopBoard) return;
@@ -27,20 +18,24 @@ function flipCard() {
 
     this.classList.add('flip');
 
+    // if hasFlippedCard variable is false, first card flipped
     if (!hasFlippedCard) {
+
         // first click
         hasFlippedCard = true;
         firstCard = this;
 
         return;
-    }
-        // second click
+    } else {
+        // if hasFlippedCard variable is true, second card flipped
         secondCard = this;
 
         checkForMatch();
+    }
 }
 
-// function that checks if the cards match & Sound
+
+// function that checks if the cards match & Sound if they do
 function checkForMatch() {
     let isMatch = firstCard.dataset.name === secondCard.dataset.name;
     let soundFlag = true;
@@ -56,7 +51,6 @@ function checkForMatch() {
 }
 
 
-
 // stop cards if it is a match
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
@@ -64,6 +58,7 @@ function disableCards() {
 
     resetBoard();
 }
+
 
 // if it is NOT a match
 function unflipCards() {
@@ -74,8 +69,10 @@ function unflipCards() {
         secondCard.classList.remove('flip');
 
     resetBoard();
+    // time of card flip
     }, 1500);
 }
+
 
 // function to reset the board to normal
 function resetBoard() {
@@ -84,6 +81,7 @@ function resetBoard() {
     firstCard = null; 
     secondCard = null;
 }
+
 
 // function to shuffle cards each new game/refresh
 (function shuffle() {
@@ -95,5 +93,18 @@ function resetBoard() {
     // bgm.play();
 })();
 
+
 // event listener to flip card on click
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+// you win!!
+function playerWon() {
+    if (!stopBoard === true) {
+        console.log("yeah");
+    }
+};
+// console.log(you won);
+
+{/* <script type="text/javascript">
+        alert("You won!");
+    </script> */}
